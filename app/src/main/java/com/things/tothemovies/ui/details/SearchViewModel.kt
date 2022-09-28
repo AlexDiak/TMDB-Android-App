bf63e749774d1fe3b95fbb898529c6a29d9d38bc
AlexDiak <alex_94@hotmail.gr>
2022-09-29 00:22:34 +0300

    Initial commit

---
+++ app/src/main/java/com/things/tothemovies/ui/details/SearchViewModel.kt
@@ -0,0 +1,7 @@
+package com.things.tothemovies.ui.details
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/things/tothemovies/ui/search/DetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.things.tothemovies.ui.search
+
+import androidx.lifecycle.ViewModel
+
+class DetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
